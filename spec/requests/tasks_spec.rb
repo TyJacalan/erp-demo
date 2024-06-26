f872e32c3b6b6186d240695c8a02780f2c5a1b40
Ty Jac <muzuuu@protonmail.com>
2024-06-26 11:05:41 +0800

    chore: add tests for task creation

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -4,8 +4,9 @@ require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :request do
   let(:user) { users(:valid_user) }
-  let(:task_manager) { users(:task_manager)}
-  let(:client) { users(:valid_client) }
+  let(:task_manager) { users(:task_manager) }
+  let(:client) { clients(:valid_client) }
+  let(:task_category) { task_categories(:valid_category) }
   let(:task) { tasks(:valid_task) }
 
   context 'when the user is a non task manager' do
@@ -60,13 +61,58 @@ RSpec.describe 'Tasks', type: :request do
       end
     end
 
+    describe 'POST /create' do
+      let(:task_params) do
+        {
+          client_id: client.id,
+          tasks: {
+            '0' => {
+              name: 'New Task',
+              hours: 5,
+              deadline: 1.week.from_now.strftime('%Y-%m-%d'),
+              task_category_id: task_category.id,
+              user_id: task_manager.id
+            }
+          }
+        }
+      end
+
+      it 'creates a new task and responds with a success toast' do
+        post tasks_path, params: task_params, headers: { 'Accept' => 'text/vnd.turbo-stream.html' }
+        expect(response.content_type).to include('text/vnd.turbo-stream.html')
+        expect(flash[:notice]).to eq(I18n.t('task.create.success'))
+      end
+    end
+
+    describe 'POST /create' do
+      let(:task_params) do
+        {
+          client_id: client.id,
+          tasks: {
+            '0' => {
+              name: 'New Task',
+              hours: 5,
+              deadline: 1.week.from_now.strftime('%Y-%m-%d')
+            }
+          }
+        }
+      end
+
+      it 'does not create a new task and responds with an error toast' do
+        post tasks_path, params: task_params, headers: { 'Accept' => 'text/vnd.turbo-stream.html' }
+        expect(response.content_type).to include('text/vnd.turbo-stream.html')
+        expect(flash[:alert]).to eq('Submission failed! Errors were found.')
+      end
+    end
+
     describe 'PATCH /update' do
       it 'updates the task and returns success' do
-        patch task_path(task), params: { task: { name: 'Updated Task' } }, headers: { 'Accept' => 'text/vnd.turbo-stream.html' }
+        patch task_path(task), params: { task: { name: 'Updated Task' } },
+                               headers: { 'Accept' => 'text/vnd.turbo-stream.html' }
         task.reload
         expect(task.name).to eq('Updated Task')
         expect(response.content_type).to include('text/vnd.turbo-stream.html')
-        expect(flash[:notice]).to eq(I18n.t("task.update.success"))
+        expect(flash[:notice]).to eq(I18n.t('task.update.success'))
       end
     end
 
@@ -75,7 +121,7 @@ RSpec.describe 'Tasks', type: :request do
         delete task_path(task), headers: { 'Accept' => 'text/vnd.turbo-stream.html' }
         expect(response.content_type).to include('text/vnd.turbo-stream.html')
         expect(Task.exists?(task.id)).to be_falsey
-        expect(flash[:notice]).to eq(I18n.t("task.destroy.success"))
+        expect(flash[:notice]).to eq(I18n.t('task.destroy.success'))
       end
     end
   end
