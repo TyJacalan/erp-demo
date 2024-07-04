007b6bcdcdcb1f206c11de758ef1b72e63da9b05
Ty Jac <muzuuu@protonmail.com>
2024-07-04 18:00:14 +0800

    chore: add mailer configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,18 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
   config.action_mailer.delivery_method = :letter_opener
 
+  # config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:host) }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   user_name: Rails.application.credentials.dig(:google_smtp_email),
+  #   password: Rails.application.credentials.dig(:google_smtp_password),
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
