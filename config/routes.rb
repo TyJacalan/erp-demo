6304e0483826025809522f2d2ac73d2de6faf2bf
Ty Jac <muzuuu@protonmail.com>
2024-07-04 16:18:40 +0800

    fix: temp remove omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   }
 
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
+    # omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     invitations: 'users/invitations'
