7b1a62308fcb56ac164a388afe4fb4dcdcd9c342
Ty Jac <muzuuu@protonmail.com>
2024-07-04 17:29:15 +0800

    chore: modify script to add more users

--- db/script/create_users.rb
+++ db/script/create_users.rb
@@ -4,6 +4,7 @@ User.find_or_create_by(email: 'clientmanager@example.com') do |user|
   user.full_name = 'Client Manager'
   user.password = 'password123!'
   user.position = :associate_development_manager
+  user.team = 'Corgis'
   user.skip_invitation
 end
 
@@ -11,5 +12,30 @@ User.find_or_create_by(email: 'taskmanager@example.com') do |user|
   user.full_name = 'Task Manager'
   user.password = 'password123!'
   user.position = :senior_development_associate
+  user.team = 'Bongos'
+  user.skip_invitation
+end
+
+User.find_or_create_by(email: 'teammanager@example.com') do |user|
+  user.full_name 'Team Manager'
+  user.password = 'password123!'
+  user.position = :associate_development_manager
+  user.team = 'Armadillos'
+  user.skip_invitation
+end
+
+User.find_or_create_by(email: 'freelancer@example.com') do |user|
+  user.full_name 'Freelancer User'
+  user.password = 'password123!'
+  user.position = :freelancer
+  user.team = 'Bongos'
+  user.skip_invitation
+end
+
+User.find_or_create_by(email: 'director@example.com') do |user|
+  user.full_name 'Freelancer User'
+  user.password = 'password123!'
+  user.position = :director
+  user.team = 'Tarsiers'
   user.skip_invitation
 end
